95f5784b4bec4d606a6e5d860c197790359ea25c
Renato Araga <raraga@gmail.com>
2026-04-27 15:03:16 -0700

    created Photo and Project models

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'slug' => fake()->slug(),
+            'description' => fake()->paragraph(3),
+            'site_url' => fake()->optional()->url(),
+            'code_url' => fake()->optional()->url(),
+        ];
+    }
+}
